7fd1198e8f2b8efa3d87b70aaa40de5ff595e4fe
Festus Omaku <fomaku3@gmail.com>
2023-08-29 17:14:47 +0100

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -15,7 +15,7 @@ def index() -> str:
     Displays the welcome message on the homepage.
     """
 
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
