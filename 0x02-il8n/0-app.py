7f4556e9d916fa973a0235d5c668a5aa617827b9
Festus Omaku <fomaku3@gmail.com>
2023-08-29 17:06:24 +0100

    app.py

---
+++ 0x02-il8n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is a simple Flask app that displays a welcome message.
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    """
+    Displays the welcome message on the homepage.
+    """
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
